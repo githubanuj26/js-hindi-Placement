8da300e46e41c420afb2dc4abd51f7dc0dda8545
githubanuj26 <129326364+githubanuj26@users.noreply.github.com>
2026-07-26 06:30:22 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+// let score = "Anuj"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+// let isLoggedIn = "Anuj"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 =>false
+// "" => false
+// "Anuj" => true
+
+
+let someNumber = 33
+
+let stringNamber = String(someNumber)
+console.log(stringNamber);
+console.log(typeof stringNamber);
+
