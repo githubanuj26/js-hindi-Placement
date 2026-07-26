e5f3f5f8957554935b58d18cb493020b6f13089e
githubanuj26 <129326364+githubanuj26@users.noreply.github.com>
2026-07-26 05:46:17 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict";  // treat all JS code as newer version
+
+
+// alert( 3 * 3)  // we are using nodejs, not browser
+
+console.log( 3 * 3 ) // code redability should be highl
+    
+    console.log("hitesh")
+
+
+    let name = "Anuj"
+    let age = 21
+    let isLoggedIn = false
+    let state = null
+// primitive data type
+    // number => 2 to power 53
+    // bigint
+    // string => ""
+    // boolearn = true/false
+    // null => standalone value 
+    // undefined => 
+    // symbol => unique
+
+
+    // object
+
+
+    console.log(typeof null)
+    console.log(typeof undefined)
+
