6c7b68ba6616f9e2a350205ca0c06a8755170689
githubanuj26 <129326364+githubanuj26@users.noreply.github.com>
2026-07-27 13:03:36 +0000

    conversionOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,4 +1,4 @@
-// let score = "Anuj"
+// let score = 33
 
 // console.log(typeof score);
 // console.log(typeof(score));
@@ -24,7 +24,63 @@
 
 let someNumber = 33
 
-let stringNamber = String(someNumber)
-console.log(stringNamber);
-console.log(typeof stringNamber);
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
 
+// **************************Operations *********************
+
+ let value = 3
+
+let neegvalue = -value
+// console.log(neegvalue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2*3);
+
+// let str1 = "hello"
+// let str2 = "hitesh"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("2" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 4)  * 5 % 3);
+
+console.log(+true);
+console.log(+"");
+
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// practices question 
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+//* not cleard
